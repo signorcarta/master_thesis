dcc5334c66481bf5039d4ff26d201c35ae942db9
Signorcarta <davide.carta.1@studenti.unipd.it>
2020-11-18 10:57:58 +0100

    Update get_size.py

--- get_size.py
+++ get_size.py
@@ -10,39 +10,34 @@ host_name=''
 port_number=''
 psw=''
 
-#______________________________________________________________________________________________________________________
+
 ## Connect to MySQL instance and query the data ##
 conn = mysql.connector.connect(user='read-only', password=psw, host=host_name, port=port_number)
 cursor = conn.cursor(dictionary=True)
 query = ("SELECT sum(data_length+index_length)/1024/1024 ‘Size’ FROM information_schema.TABLES GROUP BY table_schema;")
 cursor.execute(query) ## Execute query
 row = cursor.fetchone() ## Get first row
-#______________________________________________________________________________________________________________________
 
-#______________________________________________________________________________________________________________________
+
 ## Extract the key ##
 key = str(row.keys())
 key = key[12:16]
 #key = "\'"+ key + "\'"
 #print("\n>> The extracted key is: " + key + " <<\n")
-#______________________________________________________________________________________________________________________
 
-#______________________________________________________________________________________________________________________
 ## Fill the list with the values of DB sizes ##
 list = []
 #Get rows and fill the list with their cast to float
 while row is not None:
 	list.append(float(row.get(key))) 
 	row = cursor.fetchone()
-#______________________________________________________________________________________________________________________
 
-#______________________________________________________________________________________________________________________
 ## Get the total sum ##
 sum=0
-#Get the total sum
 for elem in list:
 	sum+=elem
-## Print size of the instance in [GB]
+	
+## Print size of the instance in [GB] ##
 print("\n>> Size of instance " + host_name + ": " + str(sum/1024) + " GB <<\n")
 
 conn.close()
