e1407b26b8f2dda8a9cf99a2b0ed679481a58086
Signorcarta <davide.carta.1@studenti.unipd.it>
2020-11-19 13:53:13 +0100

    Update check_tables.py

--- check_tables.py
+++ check_tables.py
@@ -1,64 +1,63 @@
-#
-#
-#
-#
-#
-import mysql.connector
+ports = []
+hosts = [""]
+psw == ''
 
-host_name = []
-port_number = []
+ls = zip(hosts, ports)
 
-psw = 'My3306ro!'
+for elem in ls:
 
-## Connect to the MySQL instance and query the data ##
-conn = mysql.connector.connect(user='read-only', password=psw, host=host_name, port=port_number)
-cursor = conn.cursor(dictionary=True)
-query = ("SELECT TABLE_NAME, ENGINE FROM information_schema.TABLES;")
-cursor.execute(query) # Execute query
-row = cursor.fetchone() # Fetch the first row
+        host_name = elem[0]
+        port_number = elem[1]
 
-## Get list with all tables and their engine ##
-table_name = []
-table_engine = []
-while row is not None:
-       table_name.append(row.get('TABLE_NAME'))
-       table_engine.append(row.get('ENGINE'))
-       row = cursor.fetchone()
+       ## Connect to the MySQL instance and query the data ##
+       conn = mysql.connector.connect(user='read-only', password=psw, host=host_name, port=port_number)
+       cursor = conn.cursor(dictionary=True)
+       query = ("SELECT TABLE_NAME, ENGINE FROM information_schema.TABLES;")
+       cursor.execute(query) # Execute query
+       row = cursor.fetchone() # Fetch the first row
 
-info_list = zip(table_name, table_engine)
+       ## Get list with all tables and their engine ##
+       table_name = []
+       table_engine = []
+       while row is not None:
+              table_name.append(row.get('TABLE_NAME'))
+              table_engine.append(row.get('ENGINE'))
+              row = cursor.fetchone()
 
-print("\n.\n.\n.\nExamined instance: " + host_name)
-## Separate InnoDB and non-InnoDB tables
-inno_tables = []
-other_tables = []
-nonInnoDBengine = []
-InnoDBengine = [] # This list is left empty on purpose
+       info_list = zip(table_name, table_engine)
 
-for elem in info_list:
-        engine= elem[1]
-        table = elem[0]
-        if(engine != 'InnoDB' and engine != None):
-                other_tables.append(table)
-                nonInnoDBengine.append(engine)
-        else:
-                inno_tables.append(table)
+       print("\n.\n.\n.\nExamined instance: " + host_name)
+       ## Separate InnoDB and non-InnoDB tables
+       inno_tables = []
+       other_tables = []
+       nonInnoDBengine = []
+       InnoDBengine = [] # This list is left empty on purpose
 
-nonInnoS = zip(other_tables, nonInnoDBengine)
+       for elem in info_list:
+               engine= elem[1]
+               table = elem[0]
+               if(engine != 'InnoDB' and engine != None):
+                       other_tables.append(table)
+                       nonInnoDBengine.append(engine)
+               else:
+                       inno_tables.append(table)
 
-name_file = host_name + ".txt"
-output = open(name_file, "w")
+       nonInnoS = zip(other_tables, nonInnoDBengine)
 
-## Print non-InnoDB tables ##
-for elem in nonInnoS:
-        print("\n   Table [" + str(elem[0]) + "] type is: " + elem[1], file = output)
+       name_file = host_name + ".txt"
+       output = open(name_file, "w")
 
-## Print InnoDB tables ##
-#for elem in inno_tables:
-#        print("\n   Table [" + str(elem) + "] type is InnoDB")
+       ## Print non-InnoDB tables ##
+       for elem in nonInnoS:
+               print("\n   Table [" + str(elem[0]) + "] type is: " + elem[1], file = output)
 
-print("\n###### SUMMARY ######", file = output)
-print("_____________________", file = output)
-print("| Total:      | " + str(len(table_name)) + " |", file = output)
-print("| InnoDB:     | " + str(len(inno_tables)) + " |",file = output)
-print("| Non-InnoDB: | " + str(len(other_tables)) + " |",file = output)
-print("_____________________", file = output)
+       ## Print InnoDB tables ##
+       #for elem in inno_tables:
+       #        print("\n   Table [" + str(elem) + "] type is InnoDB")
+
+       print("\n###### SUMMARY ######", file = output)
+       print("_____________________", file = output)
+       print("| Total:      | " + str(len(table_name)) + " |", file = output)
+       print("| InnoDB:     | " + str(len(inno_tables)) + " |",file = output)
+       print("| Non-InnoDB: | " + str(len(other_tables)) + " |",file = output)
+       print("_____________________", file = output)
